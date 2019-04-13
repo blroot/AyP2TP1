1dfa4548a9e2d478feea7c48911b45449c395523
Bruno Lottero <brunoelottero@gmail.com>
2019-04-12 23:12:16 -0300

    se hizo otro cambio en el mensaje que se imprime

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("hola, vengo a hacer un cambio");
+		System.out.println("hola, vengo a hacer otro cambio");
 	}
 
 }
